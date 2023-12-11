ba474032a520443e33aadb9e2243c2aa8bf3168a
ravularajesh3333 <82590850+ravularajesh3333@users.noreply.github.com>
2023-12-11 10:16:27 +0530

    Update saraswathi.sh

--- saraswathi.sh
+++ saraswathi.sh
@@ -1,1 +1,11 @@
-echo "Hello saraswathi from Karimnagar"
+pipeline {
+    agent any
+
+    stages {
+        stage('Hello') {
+            steps {
+                s3Upload consoleLogLevel: 'INFO', dontSetBuildResultOnFailure: false, dontWaitForConcurrentBuildCompletion: false, entries: [[bucket: '', excludedFile: '', flatten: false, gzipFiles: false, keepForever: false, managedArtifacts: false, noUploadOnFailure: false, selectedRegion: 'us-east-1', showDirectlyInBrowser: false, sourceFile: '**/*', storageClass: 'STANDARD', uploadFromSlave: false, useServerSideEncryption: false]], pluginFailureResultConstraint: 'FAILURE', profileName: 'ganesh', userMetadata: []
+            }
+        }
+    }
+}
